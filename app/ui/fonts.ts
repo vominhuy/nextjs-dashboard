bff1a8c95b8a81e41b4abbbceb71c594106dfdaf
Vo Minh Huy <huyminh739@gmail.com>
2024-09-24 11:50:48 +0700

    change font weight

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Lusitana } from 'next/font/google';
  
-export const inter = Inter({ weight: '900', subsets: ['latin'] });
+export const inter = Inter({ weight: '800', subsets: ['latin'] });
 export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
