cc29ea42678a3935759496ccaf7f6c3d8efd330a
Vo Minh Huy <huyminh739@gmail.com>
2024-09-24 11:46:21 +0700

    init new project with nextjs learn

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ weight: '900', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
